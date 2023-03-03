72fec2feb3c9a55aac8280aa64fc1544790c584e
David Ghibradze <96905686+Ghibrasoft@users.noreply.github.com>
2023-03-03 19:57:20 +0400

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBx1QjMKy67foZQ643EF28YRLDMcAYdSMo",
-  authDomain: "article-app-7320d.firebaseapp.com",
-  projectId: "article-app-7320d",
-  storageBucket: "article-app-7320d.appspot.com",
-  messagingSenderId: "200633612218",
-  appId: "1:200633612218:web:01aff59c2cccdaca5acf83",
+  apiKey: "XXX",
+  authDomain: "XXX",
+  projectId: "XXX",
+  storageBucket: "XXX",
+  messagingSenderId: "XXX",
+  appId: "XXX",
 };
 
 const app = initializeApp(firebaseConfig);
